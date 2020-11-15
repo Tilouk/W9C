aedfc9541c4218a4461505f8436b45347ecd9c4d
Alan <tilouk@hotmail.com>
2020-11-14 19:02:25 -0700

    Array

---
+++ app.js
@@ -0,0 +1,6 @@
+var tweets = ["tweet1", "tweet2", "tweet3", "tweet4", "tweet5", "tweet6", "tweet7", "tweet8", "tweet9", "tweet10"];
+
+for(var index = 0; index < tweets.length; index++) {
+
+    console.log(tweets[index]);
+}
